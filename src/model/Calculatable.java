42b50ee42fb2790f423b4125c4bacb1c9748bc43
yoo16 <yohei.yoshikawa@gmail.com>
2022-08-26 16:07:27 +0900

    add

---
+++ src/model/Calculatable.java
@@ -0,0 +1,10 @@
+package model;
+
+public interface Calculatable {
+
+	public void calculate();
+	public int count();
+	public void add(Item item, int amount);
+	public int calculateWithoutTaxed();
+
+}
